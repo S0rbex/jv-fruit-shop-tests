19ca8f3d1c18a10eec9bfea1d7304799f00582b5
S0rbex <vitalikmartunushun@gmail.com>
2025-12-26 19:50:11 +0200

    Adding required files

--- src/test/java/core/basesyntax/FileReaderImplTest.java
+++ src/test/java/core/basesyntax/FileReaderImplTest.java
@@ -1,4 +1,29 @@
 package core.basesyntax;
 
-public class FileReaderImplTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import service.FileReader;
+import service.FileReaderImpl;
+
+class FileReaderImplTest {
+    private static final String EXISTING_FILE = "src/test/java/resource/test_data.csv";
+    private static final String NON_EXISTENT_FILE = "src/test/java/resource/not_Real.csv";
+
+    private final FileReader fileReader = new FileReaderImpl();
+
+    @Test
+    void read_ExistingFile_Ok() {
+        List<String> lines = fileReader.read(EXISTING_FILE);
+        assertEquals(6, lines.size());
+    }
+
+    @Test
+    void read_NonExistentFile_ThrowRuntimeException() {
+        assertThrows(RuntimeException.class, () -> {
+            fileReader.read(NON_EXISTENT_FILE);
+        });
+    }
 }
--- src/test/java/core/basesyntax/FileWriterImplTest.java
+++ src/test/java/core/basesyntax/FileWriterImplTest.java
@@ -1,4 +1,41 @@
 package core.basesyntax;
 
-public class FileWriterImplTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import service.FileWriter;
+import service.FileWriterImpl;
+
+class FileWriterImplTest {
+    private static final String TEST_RESULT_FILE = "src/test/java/resource/test_output.csv";
+
+    private final FileWriter fileWriter = new FileWriterImpl();
+
+    @AfterEach
+    void tearDown() {
+        try {
+            Files.deleteIfExists(Path.of(TEST_RESULT_FILE));
+        } catch (IOException e) {
+            System.out.println("We can`t create file.");
+        }
+    }
+
+    @Test
+    void write_ContentToFile_Ok() {
+        String content = "fruit,quantity\nbanana,100";
+
+        fileWriter.write(content, TEST_RESULT_FILE);
+
+        try {
+            String actualContent = Files.readString(Path.of(TEST_RESULT_FILE));
+            assertEquals(content, actualContent);
+        } catch (IOException e) {
+            fail("File should exist and be readable: " + e.getMessage());
+        }
+    }
 }
